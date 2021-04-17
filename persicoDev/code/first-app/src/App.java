fdc44b0bafb72ac889461ab6e6943a362479afc5
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-04-17 18:03:15 +0200

    changes on code.

--- persicoDev/code/first-app/src/App.java
+++ persicoDev/code/first-app/src/App.java
@@ -1,5 +1,11 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+import java.util.Scanner;
+
+class App {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		String input;
+		input = myObj.nextLine();
+		System.out.println("The input is: " + input);
+        myObj.close();
+	}
 }
