e8d7f1fb183e32336f10d614454739b07b71dec4
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-04-17 18:31:46 +0200

    fixed java packages.

---
+++ persicoDev/code/practiceFour/src/App.java
@@ -0,0 +1,7 @@
+package persicoDev.code.practiceFour.src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
