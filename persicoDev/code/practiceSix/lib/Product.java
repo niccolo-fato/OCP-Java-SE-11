b88676b50c7945b1b87c962f1ea8e80bc1aff883
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-04-18 05:55:38 +0200

    added practiceSix.

---
+++ persicoDev/code/practiceSix/lib/Product.java
@@ -0,0 +1,52 @@
+package persicoDev.code.practiceSix.lib;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import static persicoDev.code.practiceSix.lib.Rating.*;
+
+public class Product {
+  public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+  private int id;
+  private String name;
+  private BigDecimal price;
+  private Rating rating;
+
+  public Product() {
+    this(0, "no name", BigDecimal.ZERO);
+   }
+
+  public Product(int id, String name, BigDecimal price, Rating rating) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.rating = rating;
+  }
+
+  public Product(int id, String name, BigDecimal price) {
+    this(id, name, price, NOT_RATED);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public BigDecimal getDiscount() {
+    return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+  }
+
+  public Rating getRating() {
+    return rating;
+  }
+
+  public Product applyRating(Rating newRating) {
+    return new Product(this.id, this.name, this.price, newRating);
+  }
+
+}
