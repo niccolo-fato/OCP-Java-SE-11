fb8c2fc811cefb6700314794aaff175ba355fe78
Alessio Persichetti <alessiopersichettidev@gmail.com>
2021-04-18 05:35:06 +0200

    modified practiceFive.

--- persicoDev/code/practiceFive/lib/Product.java
+++ persicoDev/code/practiceFive/lib/Product.java
@@ -1,39 +1,52 @@
 package persicoDev.code.practiceFive.lib;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import static persicoDev.code.practiceFive.lib.Rating.*;
 
 public class Product {
   public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
   private int id;
   private String name;
   private BigDecimal price;
-  // Private Rating rating;
+  private Rating rating;
+
+  public Product() {
+    this(0, "no name", BigDecimal.ZERO)
+   }
+
+  public Product(int id, String name, BigDecimal price, Rating rating) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.rating = rating;
+  }
+
+  public Product(int id, String name, BigDecimal price) {
+    this(id, name, price, NOT_RATED);
+  }
 
   public int getId() {
     return id;
-  }
-
-  public void setId(final int id) {
-    this.id = id;
   }
 
   public BigDecimal getPrice() {
     return price;
   }
 
-  public void setPrice(final BigDecimal price) {
-    this.price = price;
-  }
-
   public String getName() {
     return name;
-  }
-
-  public void setName(final String name) {
-    this.name = name;
   }
 
   public BigDecimal getDiscount() {
     return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
   }
+
+  public Rating getRating() {
+    return rating;
+  }
+
+  public Product applyRating(Rating newRating) {
+    return new Product(this.id, this.name, this.price, newRating);
+  }
+
 }
--- persicoDev/code/practiceFive/src/Shop.java
+++ persicoDev/code/practiceFive/src/Shop.java
@@ -1,13 +1,21 @@
 package persicoDev.code.practiceFive.src;
 import java.math.BigDecimal;
-import persicoDev.code.practiceFour.lib.Product;
+import persicoDev.code.practiceFive.lib.Product;
+import persicoDev.code.practiceFive.lib.Rating;
 
 public class Shop {
   public static void main(String[] args) {
-    Product p1 = new Product();
-    p1.setId(101);
-    p1.setName("Tea");
-    p1.setPrice(BigDecimal.valueOf(1.99));
-    System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount());
+    Product p1 = new Product(101, "Tea", BigDecimal.valueOf(1.99));
+    Product p2 = new Product(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+    Product p3 = new Product(103, "Cake", BigDecimal.valueOf(1.99), Rating.FIVE_STAR);
+    Product p4 = new Product();
+    Product p5 = p3.applyRating(Rating.THREE_STAR);
+    
+    System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount() + " " + p1.getRating().getStars());
+    System.out.println(p2.getId() + " " + p2.getName() + " " + p2.getPrice() + " " + p2.getDiscount() + " " + p2.getRating().getStars());
+    System.out.println(p3.getId() + " " + p3.getName() + " " + p3.getPrice() + " " + p3.getDiscount() + " " + p3.getRating().getStars());
+    System.out.println(p4.getId() + " " + p4.getName() + " " + p4.getPrice() + " " + p4.getDiscount() + " " + p4.getRating().getStars());
+    System.out.println(p5.getId() + " " + p5.getName() + " " + p5.getPrice() + " " + p5.getDiscount() + " " + p5.getRating().getStars());
+
   }
 }
